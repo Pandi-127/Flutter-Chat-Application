a40925b382e54fc7265ff4884ffad4d92d5c87bb
Pandi-127 <pandikamaraj2882@gmail.com>
2025-10-28 23:28:54 +0530

    socket 1

--- entry-server.js
+++ entry-server.js
@@ -5,16 +5,11 @@ const { Socket } = require("socket.io");
 const app = express();
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var io = require("socket.io")(server,{
-  cors:{
-    origin: "*"
-  }
-});
+var io = require("socket.io")(server);
 
 //MidleWare
-
 app.use(express.json());
 app.use(cors());
 
 io.on("connection", (Socket) => { console.log("connected"); });
-server.listen(port, () => {console.log("server started");});
+server.listen(port, "0.0.0.0",() => {console.log("server started");});
